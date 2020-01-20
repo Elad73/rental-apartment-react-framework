808e859c8285618f53621a3efe492368831e70d8
EladRon <elad.ron.g@gmail.com>
2020-01-20 17:09:01 +0200

    adding the initial boilerplate to workon header and homepage

---
+++ client/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Home Page</div>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
