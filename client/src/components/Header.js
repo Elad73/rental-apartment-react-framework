5ca6210394fbc36b9a7d46ccb406c233f14003b4
EladRon <elad.ron.g@gmail.com>
2020-02-02 13:10:05 +0200

    changing to BEM style

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,16 +5,16 @@ class Header extends React.Component {
         return (
             <div>
                 <header className="header">
-                    <div className="logo-box">
-                        <img src="../../../img/H6_Logo.png" alt="Logo" className="logo"></img>
+                    <div className="header__logo-box">
+                        <img src="../../../img/H6_Logo.png" alt="Logo" className="header__logo"></img>
                     </div>
 
-                    <div className="text-box">
+                    <div className="header__text-box">
                         <h1 className="heading-primary">
-                            <span className="heading-primary-main">Hayarkon6</span>
-                            <span className="heading-primary-sub">you can stop dreaming now</span>
+                            <span className="heading-primary--main">Hayarkon6</span>
+                            <span className="heading-primary--sub">you can stop dreaming now</span>
                         </h1>
-                        <a href="#" className="btn btn-light-gold btn-animated">Explore</a>
+                        <a href="#" className="btn btn--light-gold btn--animated">Explore</a>
                     </div>
                 </header>
             </div>
