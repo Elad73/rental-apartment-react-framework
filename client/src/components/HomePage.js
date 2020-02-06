2ec761ddfa0f518edc000bd68ac0bf37081d8f4a
EladRon <elad.ron.g@gmail.com>
2020-02-06 16:00:27 +0200

    Start building the about section to the home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,8 +3,21 @@ import React from 'react';
 class HomePage extends React.Component {
     render() {
         return (
-            <div>Home Page</div>
-          
+            <div>
+                <main>
+                    <section className="section-about">
+                        <div className="u-center-text u-margin-bottom-8">
+                            <h2 className="heading-secondary">
+                                It's all about the view
+                            </h2>
+                        </div>
+                            <div className="row">
+                            <div className="col-1-of-2">col-1-of-2</div>
+                            <div className="col-1-of-2">col-1-of-2</div>
+                        </div>
+                    </section>
+                </main>
+            </div>
             // <div>
             //     <section>
             //         <div className="row">
