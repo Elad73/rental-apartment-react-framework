2d49b393290e579d3a5611e8549605821a7d6f7d
EladRon <elad.ron.g@gmail.com>
2020-02-06 12:47:01 +0200

    adding the layout of the basic grid

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -3,9 +3,26 @@ import React from 'react';
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-                <div>Home Page</div>
-            </div>
+            <div>Home Page</div>
+          
+            // <div>
+            //     <section>
+            //         <div className="row">
+            //             <div className="col-1-of-2">col-1-of-2</div>
+            //             <div className="col-1-of-2">col-1-of-2</div>
+            //         </div>
+            //         <div className="row">
+            //             <div className="col-1-of-3">col-1-of-3</div>
+            //             <div className="col-1-of-3">col-1-of-3</div>
+            //             <div className="col-1-of-3">col-1-of-3</div>
+            //         </div>
+            //         <div className="row">
+            //             <div className="col-1-of-3">col-1-of-3</div>
+            //             <div className="col-2-of-3">col-2-of-3</div>
+            //         </div>
+            //     </section>
+            // </div>
+           
         );
     }
 }
