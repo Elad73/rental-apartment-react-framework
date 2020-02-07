6f761291ca03e1be105fcc8b704b1af46c67c6ea
EladRon <elad.ron.g@gmail.com>
2020-02-07 07:06:42 +0200

    finishing with the about section, view images, images nice effect

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -20,17 +20,13 @@ class HomePage extends React.Component {
                                 There is a fantastic view. A fully equipped Kitchen. Smart TV and strong WIFI. AC works perfectly. Washing machine & dryer.
                                 Espresso machine included as well.
                                 </p>
-                                <h3 className="heading-tertiary u-margin-bottom-small">Live adventures like you never have before</h3>
-                                <p className="paragraph">
-                                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book." 
-                                </p>
                                 <a href="#" className="btn-text">Learn more &rarr;</a>
                             </div>
                             <div className="col-1-of-2">
                                 <div className="composition">
-                                    <img src="../../../img/nat-1.jpg" alt="photo 1" className="composition__photo composition__photo--p1"></img>
-                                    <img src="../../../img/nat-2.jpg" alt="photo 2" className="composition__photo composition__photo--p2"></img>
-                                    <img src="../../../img/nat-3.jpg" alt="photo 3" className="composition__photo composition__photo--p3"></img>
+                                    <img src="../../../img/nat-2-large.jpg" alt="photo 2" className="composition__photo composition__photo--p1"></img>
+                                    <img src="../../../img/nat-3-large.jpg" alt="photo 3" className="composition__photo composition__photo--p2"></img>
+                                    <img src="../../../img/nat-1-large.jpg" alt="photo 1" className="composition__photo composition__photo--p3"></img>
                                 </div>
                             </div>
                         </div>
