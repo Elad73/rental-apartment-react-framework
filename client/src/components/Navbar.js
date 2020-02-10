68ecc4823d823aa46a9a2a1d2b179009dcbd5975
EladRon <elad.ron.g@gmail.com>
2020-02-10 17:07:27 +0200

    updating the Navbar - fininshed witout links to direct and button design

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,7 +6,7 @@ class Navbar extends React.Component {
             <div className="navigation">
                 <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
                 <label for="navi-toggle" className="navigation__button">Menu</label>   
-                <div className="navigation__backgroung">&nbsp;</div>
+                <div className="navigation__background">&nbsp;</div>
                 <nav className="navigation__nav">
                     <ul className="navigation__list">
                         <li className="navigation__item"><a href="" className="navigation__link"><span>01</span>House photos</a></li>
