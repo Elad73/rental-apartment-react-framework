c09f93d581839f901f430d7c4688397704932617
EladRon <elad.ron.g@gmail.com>
2020-02-11 06:40:36 +0200

    adding css before and after animation on the menu button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,7 +5,9 @@ class Navbar extends React.Component {
         return (
             <div className="navigation">
                 <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-                <label for="navi-toggle" className="navigation__button">Menu</label>   
+                <label for="navi-toggle" className="navigation__button">
+                    <span className="navigation__icon">&nbsp;</span>
+                </label>   
                 <div className="navigation__background">&nbsp;</div>
                 <nav className="navigation__nav">
                     <ul className="navigation__list">
