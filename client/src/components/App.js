5cd6be8e414b9e67b1adc43960345fca7d708d13
EladRon <elad.ron.g@gmail.com>
2020-02-11 15:13:11 +0200

    adding the modal design

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -19,9 +19,42 @@ const App = () => {
                     <Route path="/manual" exact component={Manual} />
                 </div>
                 <Footer />
+                <div className="modal">
+                    <div className="modal__content">
+                        <div className="modal__left">
+                            <img className="modal__img" src="../../../img/nat-4.jpg" alt="no pets"/>
+                            <img className="modal__img" src="../../../img/nat-5.jpg" alt="no smoking"/>
+                        </div>
+                        <div className="modal__right">
+                            <h2 className="heading-secondary u-margin-bottom-medium">Start booking now</h2>
+                            <h3 className="heading-tertiary u-margin-bottom-small">Important &ndash; please read these temrs before booking</h3>
+                            <p className="modal__text">
+                                dlfsgsldfgkjsldfkgj sldfgk slfkg jsldfkg sldkfg sl kjlsdk fg
+                                sdfglk sldfkg slf lsdk lsdkfjg lsdkfg lsdkf lsdkfgj lsdkfg sdfglklskf listsdlfk
+                                slfdkgj slkdf lskdf  sldfkjg slkf lskfg toweirutowirtowiru jskfjgskfdjghiertuoweirut
+                                owiertowiurtoiwur skdfjghskdjfgh wioert 
+                            </p>
+                            <a href="#" className="btn btn--light-gold">Book now</a>
+                        </div>
+                    </div>
+                </div>
             </BrowserRouter>
         </div>
     );
 }
 
 export default App;
+
+
+{/* <ul className="modal__photos-list">
+    <img className="modal__phto-item" src="../../../img/hero.jpg" alt="no pets"/>
+    <img className="modal__phto-item" src="../../../img/hero.jpg" alt="no pets"/>
+    <img className="modal__phto-item" src="../../../img/hero.jpg" alt="no pets"/>
+</ul>
+<p className="modal__text">
+    <ul className="modal__text-list">
+        <li className="modal__text-item">No pets allowed</li>
+        <li className="modal__text-item">No parties allowed</li>
+        <li className="modal__text-item">No smoking allowed</li>
+    </ul>
+</p> */}
