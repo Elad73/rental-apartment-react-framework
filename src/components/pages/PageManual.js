511b144946a849a7f3eb0dd343f8cba5a70d3ca9
EladRon <elad.ron.g@gmail.com>
2020-02-18 22:06:16 +0200

    adding the skeleton of the manual page

--- src/components/pages/PageManual.js
+++ src/components/pages/PageManual.js
@@ -3,11 +3,69 @@ import React from 'react';
 class Manual extends React.Component {
     render() {
         return (
-            <div>
+            <section className="section-manual">
+                <h1>HOUSE MANUAL and RULES</h1>
                 <main>
-                    Manual
+                    <p>Welcome to Casa de la Ron. We are so pleased you have decided to book with us and hope you have an enjoyable stay.</p>
+                    <p>
+                        Kind reminder for the house rules:
+                        -no smoking
+                        -no pets 
+                        -no parties or events
+                    </p>
+                    <p>
+                        Address:
+                        6 HaYarkon st. 6th floor. 11th app.
+                        The code to the building is #2111
+                    </p>
+                    <p>
+                        Internet
+                        network: "Casa de la Ron"
+                        password: 123456789
+                    </p>
+                    <p>
+                        Coffee machine:
+                        There are capsules at the closet near the water tap.
+                        There is a milk warming and making foam machine at the closet near the kitchen stove.
+                    </p>
+                    <p>
+                        Filtered water:
+                        There is a filtering of water machine. Black device near the curtains at the end of the kitchen.
+                        -Blue light for cold water.
+                        -"Steam" for boiling the hot water.
+                        -Red light for hot water (when the water are hot enough, after pressing the "steam" button.)
+                        -lukewarm water button, at the middle of the buttons column for lukewarm water.
+                    </p>
+                    <p>
+                        TV
+                        The apartment holds a smart TV powered and controlled by a remote (the big one) for turning on and off the TV, by the orange button.
+                        the content is being controlled by the smaller streamer remote. ("mi" remote).
+                        Activate it to get the content on the HDMI 1 TV source.
+                        The TV has Netflix installed with our personal account. Please use the "guest" account for your pleasure or logout and login into your account.
+                        The TV also has "YouTube" installed and additional music, news and sports apps.
+                    </p>
+                    <p>
+                        Sound bar
+                        The apartment is equipped with Bluetooth sound bad, right beneath the TV. You can pair your phone and hear your favorite music.
+                    </p>
+                    <p>
+                        Hot water
+                        There is 2 switches on the wall, right next to the 2nd bathroom, that activate the boiler for hot water.
+                        This is an electric boiler, activating it for 45 minutes will get you a hot water.
+                    </p>
+                    <p>
+                        Towels linens and laundry
+                        Each booking is provided with:
+                        - 1 towel per guest
+                        - 1 hand towel per guest
+                        - 1 set of linen per bed
+                        - 2 mattress covers
+                        The laundry machine and dryer are at your service and convenience. On top of the dryer there is a laundry powder, liquid conditioner and stain release spry.
+                        The activation is straight foūrward, if you still need assistance please contact me.
+                        At the end of your staying if there are non-washed items, please keep them at the floor or at the laundry basket on the 2nd bathroom.
+                    </p>
                 </main>
-            </div>
+            </section>
         );
     }
 }
